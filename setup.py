71b8421c2e014c38aefecfd6d764533e53ecc8ad
Jan Janssen <jan-janssen@users.noreply.github.com>
2022-02-04 07:40:14 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         'otpauth==1.0.1',
         'qrcode==7.3.1',
         'pyzbar==0.1.8',
-        'pillow==9.0.0'
+        'pillow==9.0.0',
     ],
     cmdclass=versioneer.get_cmdclass(),
     entry_points={
