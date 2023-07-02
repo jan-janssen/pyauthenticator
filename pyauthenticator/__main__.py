15a3f88ffc906475acb408685ab48dcc8401aa38
Jan Janßen <janssen@mpie.de>
2023-07-02 10:23:35 -0600

    Provide a more user-friendly command line interface

--- pyauthenticator/__main__.py
+++ pyauthenticator/__main__.py
@@ -17,11 +17,17 @@ def command_line_parser():
     """
     parser = argparse.ArgumentParser(prog="pyauthenticator")
     config_dict = load_config()
-    parser.add_argument(
-        "service",
-        help="Service to generate optauth code for. Available services are: "
-        + str(list_services(config_dict=config_dict)),
-    )
+    if len(config_dict) > 0:
+        parser.add_argument(
+            "service",
+            help="Service to generate optauth code for. The config file ~/.pyauthenticator contains the following services: "
+            + ", ".join(list_services(config_dict=config_dict)),
+        )
+    else:
+        parser.add_argument(
+            "service",
+            help="Service to generate optauth code for. Currently no service is defined in the ~/.pyauthenticator config file.",
+        )
     parser.add_argument(
         "-qr",
         "--qrcode",
@@ -31,7 +37,7 @@ def command_line_parser():
     parser.add_argument(
         "-a",
         "--add",
-        help="Add service by providing the qrcode png file as additional argument.",
+        help="Add service by providing the <qrcode.png> file as additional argument.",
     )
     args = parser.parse_args()
     if args.qrcode:
@@ -42,7 +48,23 @@ def command_line_parser():
         )
         print(args.service, "added.")
     else:
-        print(get_two_factor_code(key=args.service, config_dict=config_dict))
+        try:
+            print(get_two_factor_code(key=args.service, config_dict=config_dict))
+        except ValueError:
+            if len(config_dict) > 0:
+                print(
+                    'The service "'
+                    + args.service
+                    + '" does not exist.\n\nThe config file ~/.pyauthenticator contains the following services:'
+                )
+                for service in list_services(config_dict=config_dict):
+                    print("  * " + service)
+                print("\nChoose one of these or add a new service using:")
+            else:
+                print(
+                    "The config file ~/.pyauthenticator does not contain any services. To add a new service use:"
+                )
+            print("  pyauthenticator --add <qr-code.png> <servicename>\n")
 
 
 if __name__ == "__main__":
