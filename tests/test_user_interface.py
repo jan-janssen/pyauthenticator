71b7b035afc4127f4b10058cc9a5c8d3bdc20b9d
Jan Janssen <jan-janssen@users.noreply.github.com>
2025-07-09 16:52:57 +0200

    Update test_user_interface.py

--- tests/test_user_interface.py
+++ tests/test_user_interface.py
@@ -1,17 +1,13 @@
 import os
 import unittest
-from typing import Dict
 
 from pyauthenticator import get_two_factor_code, write_qrcode_to_file
 from pyauthenticator.share import config_file, expand_path, write_config
 
 
 class TestUserInterface(unittest.TestCase):
-    config_dict: Dict[str, str]
-    config_path: str
-
     @classmethod
-    def setUpClass(cls) -> None:
+    def setUpClass(cls):
         cls.config_dict = {
             "test": "otpauth://totp/Test%3A%20root%40github.com?secret=6IQXETC4ADOSMMUN&issuer=Test"
         }
@@ -21,11 +17,11 @@ class TestUserInterface(unittest.TestCase):
                 config_dict=cls.config_dict
             )
 
-    def test_get_two_factor_code(self) -> None:
+    def test_get_two_factor_code(self):
         code = get_two_factor_code(service="test")
         self.assertEqual(len(code), 6)
 
-    def test_write_qrcode_to_file(self) -> None:
+    def test_write_qrcode_to_file(self):
         write_qrcode_to_file(service="test")
         self.assertTrue(os.path.exists("test.png"))
 
