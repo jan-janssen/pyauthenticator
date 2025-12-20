a23e252438e4e24b334ec14dca96c31d8c5710b8
Jan Janßen <janssen@mpie.de>
2025-12-20 19:09:39 +0100

    black formatting

--- pyauthenticator/__init__.py
+++ pyauthenticator/__init__.py
@@ -3,7 +3,12 @@ Generate two factor authentication codes on the command line
 """
 
 from . import _version
-from pyauthenticator._user import write_qrcode_to_file, get_two_factor_code, add_two_factor_provider, list_two_factor_providers
+from pyauthenticator._user import (
+    write_qrcode_to_file,
+    get_two_factor_code,
+    add_two_factor_provider,
+    list_two_factor_providers,
+)
 
 __all__ = [
     "write_qrcode_to_file",
